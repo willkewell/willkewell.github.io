b515fea8086c138fab7ea2d460efb729908775bf
willkewell <william.kewell2@gmail.com>
2023-11-13 12:20:58 +0000

    Added Header Component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import CTA from './CTA'
+import './header.css'
+
+const Header = () => {
+  return (
+    <header>
+        <div className="container header__container">
+            <h1>Will Kewell</h1>
+            <h5 className="text-light">Computer Science Graduate</h5>
+        </div>
+    </header>
+  )
+}
+
+export default Header 
